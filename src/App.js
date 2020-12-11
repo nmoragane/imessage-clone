f07f264e7576799cf0a622cdb7442e76b33fbe7a
Nayomal Lakshitha <nmoragane@gmail.com>
2020-12-11 18:35:50 +0530

    sidebar1

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Imessage from "./Imessage";
 function App() {
   return (
     <div className="App">      
-      <h1>Lets build the iMessage clone</h1>
-
+     
       <Imessage/>      
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,28 @@
+import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
-import "./Sidebar.css"
+import "./Sidebar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import RateReviewOutlinedIcon from '@material-ui/icons/RateReviewOutlined';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
-    return <div className='sidebar'>
-        <h2>I am sidebar</h2>
+    return <div className='sidebar'>           
+        <div className="sidebar__header">
+            <Avatar className="sidebar__avatar"/>
+            <div className="sidebar__input">
+                <SearchIcon/>
+                <input placeholder="Search"/>
+            </div>
+
+            <IconButton variant="outlined" className="sidebar__inputButton">
+                <RateReviewOutlinedIcon />
+            </IconButton>
+        </div>
+
+
+        <div className="sidebar__chats">
+            <SidebarChat />
+        </div>        
     </div>;
     
 }
