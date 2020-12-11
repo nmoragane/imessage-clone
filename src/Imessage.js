a3249c9c51f5594e84eb6fcee568b164fd70b136
Nayomal Lakshitha <nmoragane@gmail.com>
2020-12-11 15:38:49 +0530

    first

---
+++ src/Imessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Imessage.css"
+import Sidebar from "./Sidebar"
+
+function Imessage() {
+    return <div className='imessage'>
+        {/* Sidebar */}
+        <Sidebar/>
+        {/* Chat */}
+    </div>;
+    
+}
+
+export default Imessage;
